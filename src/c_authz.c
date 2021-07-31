af795b2576ec1a3346e16131b07146d7de320d45
Niyamaka <niyamaka@yuneta.io>
2021-07-31 19:59:34 +0200

    quito access_token,id_token,refresh_token

--- src/c_authz.c
+++ src/c_authz.c
@@ -400,16 +400,6 @@ PRIVATE int mt_stop(hgobj gobj)
 
 /***************************************************************************
  *      Framework Method mt_authenticate
- *
- *  HACK Json Web Token must arrive in kw's attribute:
- *      1) "jwt"
- *          from browsers for example; they implement refresh of tokens.
- *
- *      2) "access_token", "refresh_token", ["id_token"]
- *          from backend, c_authz must implement the refresh
- *          "id_token" when is present it's a offline tokens with a long refresh time.
- *          If id_token is not present then access_token/refresh_token must be used.
- *
  ***************************************************************************/
 PRIVATE json_t *mt_authenticate(hgobj gobj, json_t *kw, hgobj src)
 {
